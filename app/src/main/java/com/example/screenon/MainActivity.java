5c0db51712de958ab281311ebd7d7e91a3ab9610
Shanmugavardhan <shanmugavardhan5@gmail.com>
2025-03-24 17:49:28 +0530

    feat: Initial project setup
    This commit establishes the foundational structure of the project, including:
    
    -   **Gradle configuration:** Sets up `gradlew`, `gradlew.bat`, `gradle-wrapper.properties`, `build.gradle.kts`, `settings.gradle.kts`, `gradle.properties`, and `libs.versions.toml`.
    -   **Android project structure:** Creates core project directories and files like `AndroidManifest.xml`, `MainActivity.java`, and resource directories (`drawable`, `layout`, `mipmap`, `values`, `xml`).
    -   **UI resources**: Configures initial UI with layouts, colors, themes, strings, and icons.
    - **Testing framework**: sets up basic test classes `ExampleInstrumentedTest.java` and `ExampleUnitTest.java`.
    - **App insights**: Configured AppInsights for Android Vitals and Firebase Crashlytics.
    -   **IDE configuration:** Adds necessary IDE-related files to `.idea/` to configure the project in Android Studio.
    - **Proguard**: added initial proguard rules.
    - **Backup rules**: added `backup_rules.xml` and `data_extraction_rules.xml`
    - **Fonts**: added `sf_pro.ttf` and `sf_pro_display_bold.ttf`
    - **Image**: added `jenitsu.png`
    
    These changes provide a solid base for the project, ready for feature development.

---
+++ app/src/main/java/com/example/screenon/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.screenon;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
